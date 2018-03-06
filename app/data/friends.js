ef5dcd782923411315e216efa67d8f1de8a29552
AngelaF <ADFrys@users.noreply.github.com>
2018-03-06 15:16:18 -0600

    Update friends api and links to friends api.

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,86 @@ var friendsArray = [
       3,
       1
     ]
+  },
+{
+  "name":"Lisa Simpson",
+  "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/Lisa_Simpson.png/220px-Lisa_Simpson.png",
+  "scores":[
+      2,
+      3,
+      5,
+      5,
+      2,
+      5,
+      1,
+      5,
+      3,
+      5
+    ]
+  },
+{
+  "name":"Pusheen",
+  "photo":"http://www.lifewithcats.tv/wp-content/uploads/2016/03/pusheen-1.jpg",
+  "scores":[
+      2,
+      1,
+      2,
+      5,
+      5,
+      2,
+      1,
+      1,
+      1,
+      1
+    ]
+  },
+{
+  "name":"Taz",
+  "photo":"http://www.cartoonspot.net/looney-tunes/wallpaper-images/taz-med9.jpg",
+  "scores":[
+      5,
+      3,
+      2,
+      1,
+      2,
+      2,
+      4,
+      1,
+      5,
+      5
+    ]
+  },
+  {
+  "name":"Professor Frink",
+  "photo":"https://www.selectspecs.com/blog/wp-content/uploads/2009/12/professorfrink1.gif",
+  "scores":[
+      1,
+      1,
+      5,
+      3,
+      5,
+      2,
+      1,
+      5,
+      1,
+      5
+    ]
+  },
+  {
+  "name":"Tweety Bird",
+  "photo":"https://digitalmode.net/wp-content/uploads/2017/01/tweety-bird.png",
+  "scores":[
+      3,
+      5,
+      4,
+      5,
+      3,
+      3,
+      2,
+      2,
+      5,
+      2
+    ]
   }
 ];
 
