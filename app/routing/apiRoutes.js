5ecbb55834c6c3a76ba8aa11b476fb117b922585
AngelaF <ADFrys@users.noreply.github.com>
2018-03-06 16:33:52 -0600

    Begin adding logic for checking survey results.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,3 +12,21 @@ module.exports = function(app) {
   	res.json(friendData);
   });
 };
+
+// Convert results into an array of numbers
+var newFriendArray = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+var testArray = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+var difference = [];
+var totalDifference;
+
+for (var i=0; i<newFriendArray.length; i++) {
+	difference = Math.abs(newFriendArray[i]-testArray[i]);
+	// totalDifference += i;
+	// console.log(totalDifference);
+	// console.log(sum(difference));
+}
+	// console.log(difference[0]);
+	// var totalDifference = difference.reduce((total, amount) => total + amount);
+	// console.log(totalDifference)
+
+
