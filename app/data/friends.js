379ea026f266ec6a1dd318ead4adf4f6fdd9f388
AngelaF <ADFrys@users.noreply.github.com>
2018-03-06 14:36:42 -0600

    Fix routing issue.

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friendsArray = [
+{
+  "name":"Mickey Mouse",
+  "photo":"http://www.freepngimg.com/download/cartoon/4-2-cartoon-transparent.png",
+  "scores":[
+      3,
+      5,
+      4,
+      3,
+      1,
+      4,
+      2,
+      2,
+      3,
+      1
+    ]
+  }
+];
+
+module.exports = friendsArray;
