5e33e43832d117f3fb37e4cf4bcf0bb96c4873e0
AngelaF <ADFrys@users.noreply.github.com>
2018-03-07 15:44:19 -0600

    Continue working on logic for comparing answer arrays.

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,25 +8,50 @@ module.exports = function(app) {
   });
 
     app.post("/api/friends", function(req, res) {
-  	friendData.push(req.body);
-  	res.json(friendData);
+      var newFriendSurvey = req.body;
+
+  	  friendData.push(newFriendSurvey);
+  	  res.json(friendData);
+
+
   });
 };
 
 // Convert results into an array of numbers
-var newFriendArray = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
-var testArray = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
-var difference = [];
-var totalDifference;
+var friendArray = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+var userArray = [3, 2, 6, 4, 5, 1, 2, 5, 4, 1];
+var difference;
+var totalDifference = 0;
+var closestMatch = 0;
 
-for (var i=0; i<newFriendArray.length; i++) {
-	difference = Math.abs(newFriendArray[i]-testArray[i]);
-	// totalDifference += i;
-	// console.log(totalDifference);
-	// console.log(sum(difference));
+for (var i=0; i<friendArray.length; i++) {
+  difference = Math.abs(friendArray[i]-userArray[i]);
+  totalDifference += difference;
 }
-	// console.log(difference[0]);
-	// var totalDifference = difference.reduce((total, amount) => total + amount);
-	// console.log(totalDifference)
+console.log(totalDifference);
+
+// If the total difference (for all questions) is closest to zero
+// then disply the friend
+// if (totalDifference === closestMatch) {
+  
+// }
 
 
+// var array = [];
+
+// function closest(array,num){
+//     var i=0;
+//     var minDiff=1000;
+//     var ans;
+//     for(i in array){
+//          var m=Math.abs(num-array[i]);
+//          if(m<minDiff){ 
+//                 minDiff=m; 
+//                 ans=array[i]; 
+//             }
+//       }
+//     return ans;
+// }
+
+// /*call array name and desired value to be closet */
+// alert(closest(array,88));
